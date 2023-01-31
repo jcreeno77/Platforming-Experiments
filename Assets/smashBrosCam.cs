21a512a58a24c4b4afcd743b6414691aeea847fc
jcreeno77 <jcreeno77@gmail.com>
2023-01-30 18:13:21 -0600

    details

--- Assets/smashBrosCam.cs
+++ Assets/smashBrosCam.cs
@@ -6,10 +6,13 @@ public class smashBrosCam : MonoBehaviour
 {
     [SerializeField] GameObject player1;
     [SerializeField] GameObject player2;
+    [SerializeField] float camSpeed;
+    [SerializeField] float camMinSize;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camSpeed = 5;
+        camMinSize = 4;
     }
 
     // Update is called once per frame
@@ -17,12 +20,12 @@ public class smashBrosCam : MonoBehaviour
     {
         Vector2 averageSpot = (player1.transform.position + player2.transform.position)/2;
         float distance = Vector2.Distance(player1.transform.position, player2.transform.position);
-        float x = Mathf.Lerp(transform.position.x, averageSpot.x, Time.deltaTime*5);
-        float y = Mathf.Lerp(transform.position.y, averageSpot.y, Time.deltaTime*5);
+        float x = Mathf.Lerp(transform.position.x, averageSpot.x, Time.deltaTime* camSpeed);
+        float y = Mathf.Lerp(transform.position.y, averageSpot.y, Time.deltaTime* camSpeed);
 
         transform.position = new Vector3(x, y, -10);
-        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, distance/2, Time.deltaTime*5);
-        GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize, 2, 200);
+        GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, distance/2, Time.deltaTime* camSpeed);
+        GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize, camMinSize, 200);
     }
 
 }
