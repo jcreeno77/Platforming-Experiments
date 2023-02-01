f271422589d8fc1c81e199f1df0a944ec6a9d93f
jcreeno77 <jcreeno77@gmail.com>
2023-01-31 19:39:30 -0600

    ok

--- Assets/bambooPlayerScript.cs
+++ Assets/bambooPlayerScript.cs
@@ -23,9 +23,13 @@ public class bambooPlayerScript : MonoBehaviour
         baseSpeed = 2f;
         speed = baseSpeed;
 <<<<<<< HEAD
+<<<<<<< HEAD
         dashSpeed = 20f;
         poleHeight = 5.38f;
         moveWidth = 1f;
+=======
+        dashSpeed = 8;
+>>>>>>> parent of 475497a (Added animations, arial movement, adjusted direction as well)
 =======
         dashSpeed = 8;
 >>>>>>> parent of 475497a (Added animations, arial movement, adjusted direction as well)
@@ -53,7 +57,11 @@ public class bambooPlayerScript : MonoBehaviour
                     transform.position += transform.up * Time.deltaTime * speed;
                 }
 <<<<<<< HEAD
+<<<<<<< HEAD
                 /*if (transform.localPosition.x > (poleHeight - distance) * 4f)
+=======
+                if (transform.localPosition.x > (2.553f - distance) * 4f)
+>>>>>>> parent of 475497a (Added animations, arial movement, adjusted direction as well)
 =======
                 if (transform.localPosition.x > (2.553f - distance) * 4f)
 >>>>>>> parent of 475497a (Added animations, arial movement, adjusted direction as well)
