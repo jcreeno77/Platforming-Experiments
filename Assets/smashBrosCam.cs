a12c47a38da4ba0e668d562fc124162477e3e828
GandalfGreyham <mattpark.art@gmail.com>
2023-03-01 11:56:39 -0800

    Ignore

--- Assets/smashBrosCam.cs
+++ Assets/smashBrosCam.cs
@@ -12,20 +12,25 @@ public class smashBrosCam : MonoBehaviour
     void Start()
     {
         camSpeed = 5;
-        camMinSize = 15;
+        camMinSize = -20;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 averageSpot = (player1.transform.position + player2.transform.position)/2;
+        Vector2 averageSpot = (player1.transform.position + player2.transform.position) / 2;
         float distance = Vector2.Distance(player1.transform.position, player2.transform.position);
-        float x = Mathf.Lerp(transform.position.x, averageSpot.x, Time.deltaTime* camSpeed);
-        float y = Mathf.Lerp(transform.position.y, averageSpot.y, Time.deltaTime* camSpeed);
+        float x = Mathf.Lerp(transform.position.x, averageSpot.x, Time.deltaTime * camSpeed);
+        float y = Mathf.Lerp(transform.position.y, averageSpot.y, Time.deltaTime * camSpeed);
 
-        transform.position = new Vector3(x, y, -10);
+        //took out orhtographic camera for testing need way to control zoom via perspective camera
+
+        transform.position = new Vector3(x, y, camMinSize);
+    }
+
+    /*transform.position = new Vector3(x, y, -10);
         GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, distance/2, Time.deltaTime* camSpeed);
         GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize, camMinSize, 160);
-    }
+    }*/
 
 }
